6aa2381a3888eb6740c231aef37c4dadaa0c47a1
Ivan Ilin <vanilin356@gmail.com>
2018-04-22 15:35:35 +0300

    homework5 finished

--- Lesson2/WeatherApp/app/src/main/java/com/geekbrains/ivanilin/weatherapp/CityinfoActivity.java
+++ Lesson2/WeatherApp/app/src/main/java/com/geekbrains/ivanilin/weatherapp/CityinfoActivity.java
@@ -7,8 +7,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Random;
 
 public class CityinfoActivity extends AppCompatActivity {
@@ -22,6 +26,32 @@ public class CityinfoActivity extends AppCompatActivity {
     private static Random rnd = new Random();
 
     private int currentTemp;
+    private boolean showPressure;
+    private int weatherForecastType;
+    private int currentPressure;
+    private TextView cityPressureLabel;
+    private RelativeLayout weekTempLayout;
+    private TextView day1Date;
+    private TextView day2Date;
+    private TextView day3Date;
+    private TextView day4Date;
+    private TextView day5Date;
+    private TextView day6Date;
+    private TextView day7Date;
+    private TextView day1Temp;
+    private TextView day2Temp;
+    private TextView day3Temp;
+    private TextView day4Temp;
+    private TextView day5Temp;
+    private TextView day6Temp;
+    private TextView day7Temp;
+    private TextView day1Pressure;
+    private TextView day2Pressure;
+    private TextView day3Pressure;
+    private TextView day4Pressure;
+    private TextView day5Pressure;
+    private TextView day6Pressure;
+    private TextView day7Pressure;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +59,8 @@ public class CityinfoActivity extends AppCompatActivity {
         Log.d(MainActivity.LOG_TAG, "cityInfoActivity - onCreate");
         setContentView(R.layout.activity_cityinfo);
         cityName = getIntent().getStringExtra(MainActivity.INTENT_CITY);
+        showPressure = getIntent().getBooleanExtra(MainActivity.INTENT_SHOW_PRESSURE, false);
+        weatherForecastType = getIntent().getIntExtra(MainActivity.INTENT_WEATHER_FORECAST, R.id.one_day_weather_forecast_radiobutton);
 
         initViews();
 
@@ -48,7 +80,17 @@ public class CityinfoActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 currentTemp = (rnd.nextInt(20)) - 10;
-                cityTempLabel.setText(getString(R.string.show_temp_template, cityName, currentTemp));
+                cityTempLabel.setText(getString(R.string.show_temp_template, currentTemp));
+
+                if (showPressure) {
+                    currentPressure = (rnd.nextInt(40)) + 740;
+                    cityPressureLabel.setText(getString(R.string.show_pressure_template, currentPressure));
+                }
+
+                if (weatherForecastType == R.id.week_weather_forecast_radiobutton){
+                    weekTempLoad();
+                }
+
                 Intent resultIntent = getIntent();
                 resultIntent.putExtra(MainActivity.CURRENT_CITY_TEMP, currentTemp);
                 setResult(RESULT_OK, resultIntent);
@@ -66,14 +108,115 @@ public class CityinfoActivity extends AppCompatActivity {
         });
     }
 
+    private void weekTempLoad() {
+        weekTempLayout.setVisibility(RelativeLayout.VISIBLE);
+        //day1
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_YEAR, 1);
+        currentTemp = (rnd.nextInt(20)) - 10;
+        day1Date.setText(new SimpleDateFormat("dd/MM").format(calendar.getTime()));
+        day1Temp.setText(getString(R.string.show_temp_template_short, currentTemp));
+        if (showPressure) {
+            currentPressure = (rnd.nextInt(40)) + 740;
+            day1Pressure.setText(String.valueOf(currentPressure));
+        }
+        //day2
+        calendar.add(Calendar.DAY_OF_YEAR, 2);
+        currentTemp = (rnd.nextInt(20)) - 10;
+        day2Date.setText(new SimpleDateFormat("dd/MM").format(calendar.getTime()));
+        day2Temp.setText(getString(R.string.show_temp_template_short, currentTemp));
+        if (showPressure) {
+            currentPressure = (rnd.nextInt(40)) + 740;
+            day2Pressure.setText(String.valueOf(currentPressure));
+        }
+
+        //day3
+        calendar.add(Calendar.DAY_OF_YEAR, 3);
+        currentTemp = (rnd.nextInt(20)) - 10;
+        day3Date.setText(new SimpleDateFormat("dd/MM").format(calendar.getTime()));
+        day3Temp.setText(getString(R.string.show_temp_template_short, currentTemp));
+        if (showPressure) {
+            currentPressure = (rnd.nextInt(40)) + 740;
+            day3Pressure.setText(String.valueOf(currentPressure));
+        }
+
+        //day4
+        calendar.add(Calendar.DAY_OF_YEAR, 4);
+        currentTemp = (rnd.nextInt(20)) - 10;
+        day4Date.setText(new SimpleDateFormat("dd/MM").format(calendar.getTime()));
+        day4Temp.setText(getString(R.string.show_temp_template_short, currentTemp));
+        if (showPressure) {
+            currentPressure = (rnd.nextInt(40)) + 740;
+            day4Pressure.setText(String.valueOf(currentPressure));
+        }
+
+        //day5
+        calendar.add(Calendar.DAY_OF_YEAR, 5);
+        currentTemp = (rnd.nextInt(20)) - 10;
+        day5Date.setText(new SimpleDateFormat("dd/MM").format(calendar.getTime()));
+        day5Temp.setText(getString(R.string.show_temp_template_short, currentTemp));
+        if (showPressure) {
+            currentPressure = (rnd.nextInt(40)) + 740;
+            day5Pressure.setText(String.valueOf(currentPressure));
+        }
+
+        //day6
+        calendar.add(Calendar.DAY_OF_YEAR, 6);
+        currentTemp = (rnd.nextInt(20)) - 10;
+        day6Date.setText(new SimpleDateFormat("dd/MM").format(calendar.getTime()));
+        day6Temp.setText(getString(R.string.show_temp_template_short, currentTemp));
+        if (showPressure) {
+            currentPressure = (rnd.nextInt(40)) + 740;
+            day6Pressure.setText(String.valueOf(currentPressure));
+        }
+
+        //day7
+        calendar.add(Calendar.DAY_OF_YEAR, 7);
+        currentTemp = (rnd.nextInt(20)) - 10;
+        day7Date.setText(new SimpleDateFormat("dd/MM").format(calendar.getTime()));
+        day7Temp.setText(getString(R.string.show_temp_template_short, currentTemp));
+        if (showPressure) {
+            currentPressure = (rnd.nextInt(40)) + 740;
+            day7Pressure.setText(String.valueOf(currentPressure));
+        }
+    }
+
     private void initViews() {
         cityNameLabel = (TextView)findViewById(R.id.city_name_label);
         cityNameLabel.setText(cityName);
 
         cityTempLabel = (TextView)findViewById(R.id.city_temp_textview);
+        cityPressureLabel = (TextView)findViewById(R.id.city_pressure_textview);
 
         showCityTempButton = (Button)findViewById(R.id.show_temp_button);
         shareWithFriendButton = (Button)findViewById(R.id.share_with_button);
+
+        weekTempLayout = (RelativeLayout)findViewById(R.id.week_temperature_layout);
+
+        day1Date = (TextView)findViewById(R.id.day_1_date);
+        day2Date = (TextView)findViewById(R.id.day_2_date);
+        day3Date = (TextView)findViewById(R.id.day_3_date);
+        day4Date = (TextView)findViewById(R.id.day_4_date);
+        day5Date = (TextView)findViewById(R.id.day_5_date);
+        day6Date = (TextView)findViewById(R.id.day_6_date);
+        day7Date = (TextView)findViewById(R.id.day_7_date);
+
+        day1Temp = (TextView)findViewById(R.id.day_1_temp);
+        day2Temp = (TextView)findViewById(R.id.day_2_temp);
+        day3Temp = (TextView)findViewById(R.id.day_3_temp);
+        day4Temp = (TextView)findViewById(R.id.day_4_temp);
+        day5Temp = (TextView)findViewById(R.id.day_5_temp);
+        day6Temp = (TextView)findViewById(R.id.day_6_temp);
+        day7Temp = (TextView)findViewById(R.id.day_7_temp);
+
+        day1Pressure = (TextView)findViewById(R.id.day_1_pressure);
+        day2Pressure = (TextView)findViewById(R.id.day_2_pressure);
+        day3Pressure = (TextView)findViewById(R.id.day_3_pressure);
+        day4Pressure = (TextView)findViewById(R.id.day_4_pressure);
+        day5Pressure = (TextView)findViewById(R.id.day_5_pressure);
+        day6Pressure = (TextView)findViewById(R.id.day_6_pressure);
+        day7Pressure = (TextView)findViewById(R.id.day_7_pressure);
+
     }
 
     @Override
--- Lesson2/WeatherApp/app/src/main/java/com/geekbrains/ivanilin/weatherapp/MainActivity.java
+++ Lesson2/WeatherApp/app/src/main/java/com/geekbrains/ivanilin/weatherapp/MainActivity.java
@@ -9,6 +9,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,12 +20,19 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     public static final String PREF_NAME = "com.geekbrains.ivanilin.weatherapp_pref";
     public static final String PREF_LAST_CITY = "weatherapp_last_city";
+    public static final String PREF_WEATHER_FORECAST = "weatherapp_weather_forecast";
+    public static final String PREF_SHOW_PRESSURE = "weatherapp_show_pressure";
+
     public static final String INTENT_CITY = "weatherapp_intent_city";
+    public static final String INTENT_WEATHER_FORECAST = "weatherapp_intent_weather_forecast";
+    public static final String INTENT_SHOW_PRESSURE = "weatherapp_intent_show_pressure";
+
     public static final String CURRENT_CITY_TEMP = "currentCityTemp";
     public static final String CURRENT_CITY_TEMP2 = "currentCityTemp2";
     public static final int REQUEST_CODE = 103;
 
     public static final String LOG_TAG = "weatherAppLogTag";
+
 
     SharedPreferences sharedPreferences;
 
@@ -32,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
     private Button callWrongIntentButton;
     private TextView tempSignTextView;
     private TextView currentCityTemp;
+    private RadioGroup forecastTypeRadioGroup;
+    private CheckBox showPressureCheckBox;
 
 
     @Override
@@ -42,12 +53,7 @@ public class MainActivity extends AppCompatActivity {
 
         initViews();
 
-        sharedPreferences = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
-
-        if (sharedPreferences.contains(PREF_LAST_CITY)){
-            citySpinner.setSelection(sharedPreferences.getInt(PREF_LAST_CITY, 0));
-        }
-
+        loadPreferences();
 
         if (savedInstanceState != null){
             Log.d(LOG_TAG, "mainActivity - onSaveInsctanceState Read, value: " + savedInstanceState.getString(CURRENT_CITY_TEMP2));
@@ -55,6 +61,22 @@ public class MainActivity extends AppCompatActivity {
         }
 
         setListeners();
+    }
+
+    private void loadPreferences() {
+        sharedPreferences = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+
+        if (sharedPreferences.contains(PREF_LAST_CITY)){
+            citySpinner.setSelection(sharedPreferences.getInt(PREF_LAST_CITY, 0));
+        }
+
+        if (sharedPreferences.contains(PREF_WEATHER_FORECAST)){
+            forecastTypeRadioGroup.check(sharedPreferences.getInt(PREF_WEATHER_FORECAST, R.id.one_day_weather_forecast_radiobutton));
+        }
+
+        if (sharedPreferences.contains(PREF_SHOW_PRESSURE)){
+            showPressureCheckBox.setChecked(sharedPreferences.getBoolean(PREF_SHOW_PRESSURE, false));
+        }
     }
 
     @Override
@@ -126,6 +148,8 @@ public class MainActivity extends AppCompatActivity {
 
                 Intent intent = new Intent(MainActivity.this, CityinfoActivity.class);
                 intent.putExtra(INTENT_CITY, city);
+                intent.putExtra(INTENT_WEATHER_FORECAST, forecastTypeRadioGroup.getCheckedRadioButtonId());
+                intent.putExtra(INTENT_SHOW_PRESSURE, showPressureCheckBox.isChecked());
                 //startActivity(intent);
                 startActivityForResult(intent, REQUEST_CODE);
             }
@@ -148,18 +172,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        citySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                //tempSignTextView.setVisibility(TextView.INVISIBLE);
-                //currentCityTemp.setVisibility(TextView.INVISIBLE);
-            }
+//        citySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+//                tempSignTextView.setVisibility(TextView.INVISIBLE);
+//                currentCityTemp.setVisibility(TextView.INVISIBLE);
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> adapterView) {
+//
+//            }
+//        });
 
+        forecastTypeRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putInt(PREF_WEATHER_FORECAST, checkedId);
+                editor.apply();
             }
         });
+
+        showPressureCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean(PREF_SHOW_PRESSURE, showPressureCheckBox.isChecked());
+                editor.apply();
+            }
+        });
+
     }
 
     private void initViews(){
@@ -168,5 +211,8 @@ public class MainActivity extends AppCompatActivity {
         callWrongIntentButton = (Button)findViewById(R.id.сall_wrong_intent_button);
         tempSignTextView = (TextView)findViewById(R.id.temp_sign);
         currentCityTemp = (TextView)findViewById(R.id.current_city_temp);
+        forecastTypeRadioGroup = (RadioGroup)findViewById(R.id.weather_forecast_type_radiogroup);
+        forecastTypeRadioGroup.check(R.id.one_day_weather_forecast_radiobutton);
+        showPressureCheckBox = (CheckBox)findViewById(R.id.show_pressure_checkbox);
     }
 }
